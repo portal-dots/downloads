0e1ba86eeae8473da33955f09f261ab805beba72
Soji <6687653+SofPyon@users.noreply.github.com>
2021-11-27 20:17:27 +0900

    リリース情報ページの表記をアップデート

--- resources/views/releases/index.blade.php
+++ resources/views/releases/index.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PortalDots {{ $major_version }} Releases</title>
+    <style>
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            line-height: 1.4;
+        }
+    </style>
 </head>
 <body>
-    <h1>PortalDots {{ $major_version }} Releases</h1>
+    <h1>PortalDots {{ $major_version }} からアップデート可能なリリース</h1>
+    <p>下記のバージョンのうち、現在お使いの PortalDots より新しいバージョンにアップデートできます。</p>
     <ul>
         @foreach ($releases as $release)
             <li>
